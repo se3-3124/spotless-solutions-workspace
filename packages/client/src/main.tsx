d0a10f08349bc580e065d2a83471256ab63213b4
ohana-matsumae <76694828+ohana-matsumae@users.noreply.github.com>
2023-11-17 21:37:26 +0900

    use tailwind

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+      <h1 className="text-3xl font-bold underline">
+          Hello world!
+      </h1>
   </React.StrictMode>,
 )
