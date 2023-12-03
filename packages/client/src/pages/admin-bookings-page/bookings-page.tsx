b3651875c5f76ee2d62b62867483ff23996276a9
candace1818 <candacehyacinth.yu-18@cpu.edu.ph>
2023-12-03 17:47:30 +0800

    Added bookings page file

---
+++ packages/client/src/pages/admin-bookings-page/bookings-page.tsx
@@ -0,0 +1,10 @@
+import './bookings-page.css';
+import PageContentCommons from '../../Components/PageContentCommons.tsx';
+
+export default function bookings() {
+    return (
+      <PageContentCommons active={0}>
+        
+      </PageContentCommons>
+    )
+}
